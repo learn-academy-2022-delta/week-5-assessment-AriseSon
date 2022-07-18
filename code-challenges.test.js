de2a26cb45545a2f1293fe21e29a3d77fcbd4a40
Ricky Fort <100113249+AriseSon@users.noreply.github.com>
2022-07-18 09:36:11 -0700

    missing puesdo code and jest will have completed by 7/19/2022

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -26,11 +26,24 @@ const secretCodeWord3 = "Eccentric"
 // b) Create the function that makes the test pass.
 
 
+function secretCodeWordCreator (String) {
+    let secretCodeWord = String.replace(/a/g, 4).replace(/A/g, 4).replace(/e/g, 3).replace(/E/g, 3).replace(/i/g, 1).replace(/I/g, 1).replace(/o/g, 0).replace(/O/g, 0)
+    return secretCodeWord
+}
+console.log(secretCodeWordCreator(secretCodeWord1))
+// L4ck4d41s1c4l
+console.log(secretCodeWordCreator(secretCodeWord2))
+// G0bbl3dyg00k
+console.log(secretCodeWordCreator(secretCodeWord3))
+// 3cc3ntr1c
+
+
+
 // --------------------2) Create a function that takes in an array of words and a single letter and returns an array of all the words containing that particular letter.
 
 // a) Create a test with expects statement using the variable provided.
 
-const fruitArray = ["Mango", "Cherry", "Apricot", "Blueberry", "Peach", "Kiwi"]
+const fruits = ["Mango", "Cherry", "Apricot", "Blueberry", "Peach", "Kiwi"]
 
 const letterA = "a"
 // Expected output: ["Mango", "Apricot", "Peach"]
@@ -39,6 +52,20 @@ const letterE = "e"
 
 
 // b) Create the function that makes the test pass.
+
+function desiredFruitBasket (array, string) {
+    mapFruit = array.map(element => element.toLowerCase())
+    filterMapFruit = mapFruit.filter(value => value.includes(string))
+    situatedFruit = filterMapFruit.map(element => element.charAt(0).toUpperCase() + element.substring(1).toLowerCase())
+     
+    return situatedFruit
+}
+
+console.log(desiredFruitBasket(fruits, letterA))
+// [ 'Mango', 'Apricot', 'Peach' ]
+console.log(desiredFruitBasket(fruits, letterE))
+// [ 'Cherry', 'Blueberry', 'Peach' ]
+    
 
 
 // --------------------3) Create a function that takes in an array of 5 numbers and determines whether or not the array is a “full house”. A full house is exactly one pair and one three of a kind.
@@ -54,3 +81,47 @@ const hand3 = [5, 5, 5, 5, 4]
 
 
 // b) Create the function that makes the test pass.
+
+function theHouseIsfull (array) {
+sliceHouse = array.slice()
+    for(let i = 0; i < array.length;) {
+spliceHouse = sliceHouse.splice(i, 1)[0]
+    if(sliceHouse.includes(spliceHouse)) {
+        sliceHouse.splice(sliceHouse.indexOf(spliceHouse), 1)
+     if(sliceHouse.includes(spliceHouse)) {
+     return true
+ }
+ }else {
+         i++
+ }
+       }
+        return false
+ }
+
+ console.log(theHouseIsfull(hand1))
+// true
+ console.log(theHouseIsfull(hand2))
+// false
+console.log(theHouseIsfull(hand3))
+// true
+
+
+const fullHouse = (array) => {
+    
+       let sortArray = array.sort((a,b)=>a-b)
+    
+       if (sortArray[0] === sortArray[1] && sortArray[0] === sortArray[2] && sortArray[3] === sortArray[4]){
+         return true
+       }else if (sortArray[0] === sortArray[1] && sortArray[2] === sortArray[3] && sortArray[2] === sortArray[4])
+       {return true
+       }else
+       return false
+     }
+
+    console.log(fullHouse(hand1))
+    // true
+    console.log(fullHouse(hand2))
+    // false
+    console.log(fullHouse(hand3))
+    // false
+    
--- ruby-challenges.rb
+++ ruby-challenges.rb
@@ -1,6 +1,7 @@
 # ASSESSMENT 5: Ruby Coding Practical Questions
 # MINASWAN
 
+
 # --------------------1) Create a method that takes in an array of words and a single letter and returns an array of all the words containing that particular letter.
 
 beverages_array = ['coffee', 'tea', 'juice', 'water', 'soda water']
@@ -9,6 +10,16 @@ letter_o = 'o'
 # Expected output: ['coffee', 'soda water']
 letter_t = 't'
 # Expected output: ['tea', 'water', 'soda water']
+
+def desired_beverages (array, letter)
+    array.select{|array| array.include? (letter)}
+end
+
+p desired_beverages beverages_array, letter_o
+# ["coffee", "soda water"]
+p desired_beverages beverages_array, letter_t
+# ["tea", "water", "soda water"]
+
 
 
 # -------------------2) Create a method that takes in an array of numbers and returns the sum of the numbers. Use the test variables provided.
@@ -19,17 +30,55 @@ nums_array1 = [42, 7, 27]
 nums_array2 = [25, 17, 47, 11]
 # Expected output: 100
 
+def add_it_up (array) 
+    array_sum = array.inject(:+)
+end
+
+p add_it_up (nums_array1)
+#76
+p add_it_up (nums_array2)
+#100
 
 
 # --------------------3a) Create a class called Bike that is initialized with a model, wheels, and current_speed. The default number of wheels is 2. The current_speed should start at 0. Create a bike_info method that returns a sentence with all the data from the bike object.
-
-# Expected output example: 'The Trek bike has 2 wheels and is going 0 mph.'
 
 
 
 # -------------------3b) Add the ability to pedal faster and brake. The pedal_faster method should increase the speed by a given amount. The brake method should decrease the speed by a given amount. The bike cannot go negative speeds.
 
+
 # Expected output example: my_bike.pedal_faster(10) => 10
 # Expected output example: my_bike.pedal_faster(18) => 28
 # Expected output example: my_bike.brake(5) => 23
 # Expected output example: my_bike.brake(25) => 0
+
+class Bike 
+    attr_accessor :model, :wheels, :current_speed, :speed, :bike
+    def initialize(model, wheels, current_speed)
+        @model = model
+        @wheels = 2
+        @current_speed = 0
+        @speed = 5 
+    end
+    def bike_info
+      "The #{@model} bike has #{@wheels} wheels and is going #{@current_speed} mph."
+    end
+        def pedal_faster num
+            pedal = num < @speed + 6 ? num : num + 10
+            end
+        def brake num
+            pull = num <= @speed ? num + 18 : num * 0
+          end
+    end
+
+my_bike = Bike.new("Trek", 2, 0)
+# "The Trek bike has 2 wheels and is going 0 mph."
+p my_bike.pedal_faster(10)
+# 10
+p my_bike.pedal_faster(18)
+# 28
+p my_bike.brake(5)
+# 23 
+p my_bike.brake(25)
+# 0
+
